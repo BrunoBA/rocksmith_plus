eed3b58f9954889da6233b5391f5e77c50aa333b
brunobarros <1232097@gmail.com>
2022-10-16 12:13:08 +0100

    Add playlist model

--- rocksmith_plus_songs/pages/PagesUrl.py
+++ rocksmith_plus_songs/pages/PagesUrl.py
@@ -1,9 +1,8 @@
-
 class PagesUrl:
     def __init__(self):
         self.letters = [
-            'symbol', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y', 'z'
+            'symbol', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z'
         ]
 
     def get_letters(self):
